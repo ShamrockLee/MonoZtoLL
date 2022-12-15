a9855dd6196c1a07619c4231f33177d412873843
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-12-16 00:09:04 +0100

    mergeSameRegion.sh: decouple with inputListSpace

--- optimization/mergeSameRegion.sh
+++ optimization/mergeSameRegion.sh
@@ -3,8 +3,6 @@
 source ./common_header.sh
 
 originalListSpaceName=SkimmedFiles_2017Background_Lists
-originalListSpace="/afs/cern.ch/work/y/yuehshun/private/Projects/fasyakhuza/MonoZtoLL/optimization/${originalListSpaceName}"
-# originalListSpace=./ntuple_filelist
 outputSpace=/eos/user/y/yuehshun/${originalListSpaceName}_output.tmp
 outputMergedSpace=/eos/user/y/yuehshun/${originalListSpaceName}_outputmerged.tmp
 
@@ -33,10 +31,6 @@ END_OF_HELP
 			;;
 		--mindepth)
 			minDepth="$2"
-			shift 2
-			;;
-		--original-list-space)
-			originalListSpace="$2"
 			shift 2
 			;;
 		--output-merged-space)
@@ -84,29 +78,24 @@ if (( force )); then
 fi
 
 declare -a searchCommand=()
-# If subDirRoot is empty, it will find "$originalListSpace"
-searchCommand+=( find "$originalListSpace${subDirRoot:+/$subDirRoot}" )
+# If subDirRoot is empty, it will find "$outputSpace"
+searchCommand+=( find "$outputSpace${subDirRoot:+/$subDirRoot}" )
 if [[ -n "${minDepth:-}" ]]; then
 	searchCommand+=( "-mindepth" "$minDepth" )
 fi
 if [[ -n "${maxDepth:-}" ]]; then
 	searchCommand+=( "-maxdepth" "$maxDepth" )
 fi
-searchCommand+=( "-name" "*.txt" )
+searchCommand+=( "-type" "d" )
 searchCommand+=( "-print0" )
 
-while IFS= read -r -d $'\0' pathOriginalList; do
-	# Change the prefix and remove the .txt file extension
-	subDirPlusPrefSlash="${pathOriginalList:${#originalListSpace}:-4}"
+while IFS= read -r -d $'\0' pathOutputDir; do
 	declare -a filesToMerge=()
-	if [[ ! -d "$outputSpace$subDirPlusPrefSlash" && ! ( -L "$outputSpace$subDirPlusPrefSlash" && -d "$(realpath "$outputSpace$subDirPlusPrefSlash")" ) ]]; then
-		echo "$outputSpace$subDirPlusPrefSlash is not a directory / a symlink to a directory" >&2
-		# exit 1
-	fi
-	filesToMerge=()
 	while IFS= read -r -d $'\0' file; do
 		filesToMerge+=( "$file" )
-	done < <(find "$outputSpace$subDirPlusPrefSlash" -mindepth 1 -maxdepth 1 -name "*.root" -print0)
-	wrapDryRun mkdir -p "$(dirname "${outputMergedSpace}${subDirPlusPrefSlash}_merged.root")"
-	wrapDryRun hadd "${forceArgArray[@]}" "${outputMergedSpace}${subDirPlusPrefSlash}_merged.root" "${filesToMerge[@]}"
+	done < <(find "$pathOutputDir" -mindepth 1 -maxdepth 1 -name "*.root" -print0)
+  (( ${#filesToMerge[@]} )) || continue;
+  pathDest="${outputMergedSpace}${pathOutputDir:${#outputSpace}}_merged.root"
+  wrapDryRun mkdir -p "$(dirname "$pathDest")"
+	wrapDryRun hadd "${forceArgArray[@]}" "$pathDest" "${filesToMerge[@]}"
 done < <("${searchCommand[@]}")
